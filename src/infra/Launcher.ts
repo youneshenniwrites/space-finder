01ea521348803dcefde774378aa23cd8f77102ae
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-14 11:55:59 +0100

    deply a basic lambda to aws

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -1,6 +1,8 @@
 import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/DataStack";
+import { LambdaStack } from "./stacks/LambdaStack";
 
 const app = new App();
 
 new DataStack(app, "DataStack");
+new LambdaStack(app, "LambdaStack");
---
+++ src/services/hello.js
@@ -0,0 +1,6 @@
+exports.main = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+};
