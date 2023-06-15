105366ebb4177471721a297a93d98ea62f943f75
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-15 13:05:31 +0100

    add more permission to spaces lambda

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -30,7 +30,13 @@ export class LambdaStack extends Stack {
       new PolicyStatement({
         effect: Effect.ALLOW,
         resources: [props.spacesTable.tableArn],
-        actions: ["dynamodb:PutItem"],
+        actions: [
+          "dynamodb:PutItem",
+          "dynamodb:Scan",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+        ],
       })
     );
 
